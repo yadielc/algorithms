50ee87cca9d019d4322e00dd2106391203636fc3
Yadiel <ycabr035@fiu.edu>
2018-06-16 19:51:15 -0400

    added simpleArraySum

---
+++ hackerRank/simpleArraySum.java
@@ -0,0 +1,15 @@
+static int simpleArraySum(int[] ar) {
+        /*
+         * Write your code here.
+         */
+
+        int sum = 0;
+
+        for (int i = 0; i < ar.length; i++){
+            sum += ar[i];
+
+        }
+
+        return sum;
+
+    }
