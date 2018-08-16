141a77c64f9e480017413b45e5f6813a1fbd8cfc
Yadiel <ycabr035@fiu.edu>
2018-08-16 17:10:57 -0400

    added leap year problem

--- hackerRank/python/leap.py
+++ hackerRank/python/leap.py
@@ -2,7 +2,7 @@
 leap.py
 
 You are given the year, and you have to write a function to check if the year is leap or not.
-Note that you have to complete the function and remaining code is given as template.
+
 
 '''
 def is_leap(year):
