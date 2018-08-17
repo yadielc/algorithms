aa6fae3dd36362dcef23d4b5c73b14cc70b782dc
Yadiel <ycabr035@fiu.edu>
2018-08-17 16:35:09 -0400

    solved runnerup problem

---
+++ hackerRank/python/runnerup.py
@@ -0,0 +1,19 @@
+'''
+runnerup.py
+
+'''
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    max = arr[0]
+    runnerUp = -1
+    index = 1
+    while index < len(arr):
+        if (arr[index] != runnerUp and arr[index] != max):
+            if arr[index] > max:
+                runnerUp = max
+                max = arr[index]
+            elif arr[index] > runnerUp:
+                runnerUp= arr[index]
+        index += 1
+    print(runnerUp)
