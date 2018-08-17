9013da24cef34b9b5140ce537229f44fcc8cbcb2
Yadiel <ycabr035@fiu.edu>
2018-08-17 15:57:05 -0400

    fixed error

--- hackerRank/python/leap.py
+++ hackerRank/python/leap.py
@@ -2,7 +2,6 @@
 leap.py
 
 You are given the year, and you have to write a function to check if the year is leap or not.
-
 '''
 def is_leap(year):
     leap = False
@@ -11,4 +10,3 @@ def is_leap(year):
     elif(year % 4 == 0 and year % 100 != 0):
         leap = True
     return leap
-    
