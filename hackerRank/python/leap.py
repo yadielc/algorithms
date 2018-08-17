1a4cd2f0967d98391e16f54ea8d86e093e357ada
Yadiel <ycabr035@fiu.edu>
2018-08-17 15:58:42 -0400

    fixed error

--- hackerRank/python/leap.py
+++ hackerRank/python/leap.py
@@ -8,7 +8,7 @@ def is_leap(year):
     # if year is evenly divisible by 400, then it's a leap year
     if(year % 400 == 0):
         leap = True
-    # if year is evenly divisible by 4, but not by 100 it's not a leap year 
+    # if year is evenly divisible by 4, but not by 100 it's a leap year 
     elif(year % 4 == 0 and year % 100 != 0):
         leap = True
     return leap
