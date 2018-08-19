5d94bc3e061a4449762f5316cd28d20eecb3b1fb
Yadiel <ycabr035@fiu.edu>
2018-08-18 20:25:53 -0400

    solved list problem

--- hackerRank/python/lists.py
+++ hackerRank/python/lists.py
@@ -20,7 +20,6 @@ reverse: Reverse the list.
 Initialize your list and read in the value of  followed by  lines of
 commands where each command will be of the  types listed above.
 Iterate through each command in order and perform the corresponding operation on your list.
-
 '''
 list = []
 
