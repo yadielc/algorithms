31ab93a1718166a432ca49e514f93dad8c47335f
Yadiel <ycabr035@fiu.edu>
2018-08-18 20:22:02 -0400

    solved list problem

--- hackerRank/python/lists.py
+++ hackerRank/python/lists.py
@@ -11,9 +11,14 @@ print: Print the list.
 remove e: Delete the first occurrence of integer .
 append e: Insert integer  at the end of the list.
 sort: Sort the list.
+
 pop: Pop the last element from the list.
+
 reverse: Reverse the list.
-Initialize your list and read in the value of  followed by  lines of commands where each command will be of the  types listed above. Iterate through each command in order and perform the corresponding operation on your list.
+
+Initialize your list and read in the value of  followed by  lines of
+commands where each command will be of the  types listed above.
+Iterate through each command in order and perform the corresponding operation on your list.
 
 '''
 list = []
