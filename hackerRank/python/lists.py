17360bcb04c4c8efe73f0b597a1bdd312e8d6a9e
Yadiel <ycabr035@fiu.edu>
2018-08-18 20:25:00 -0400

    solved list problem

--- hackerRank/python/lists.py
+++ hackerRank/python/lists.py
@@ -12,13 +12,9 @@ insert i e: Insert integer  at position .
 print: Print the list.
 
 remove e: Delete the first occurrence of integer .
-
 append e: Insert integer  at the end of the list.
-
 sort: Sort the list.
-
 pop: Pop the last element from the list.
-
 reverse: Reverse the list.
 
 Initialize your list and read in the value of  followed by  lines of
