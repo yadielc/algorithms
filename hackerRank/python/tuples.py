1efa9397d76951d528684e53e481fc279efce138
Yadiel <ycabr035@fiu.edu>
2018-08-21 13:47:13 -0400

    tuples  problem solution

--- hackerRank/python/tuples.py
+++ hackerRank/python/tuples.py
@@ -4,9 +4,12 @@ tuples.py
 Tuples Challenge on HackeRrank python
 https://www.hackerrank.com/challenges/python-tuples
 
+Given an integer, n , and n space-separated integers as input, create a tuple, , of those  integers. Then compute and print the result of .
+
+Note: uses Python 2
 '''
 if __name__ == '__main__':
-    n = int(raw_input())
+    n = int(raw_input()) # get input from user
     t = tuple(map(int, raw_input().split()))
 
 print hash(t)
