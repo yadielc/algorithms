9c611cebee016f707afeea5595bdd58af27f2243
Yadiel <ycabr035@fiu.edu>
2018-08-22 16:06:04 -0400

    added swapcase solution

--- hackerRank/python/swapcase.py
+++ hackerRank/python/swapcase.py
@@ -3,6 +3,8 @@ swapcase.py
 
 sWAP cASE: HackerRank Problem Solution
 
+You are given a string and your task is to swap cases.
+In other words, convert all lowercase letters to uppercase letters and vice versa.
 '''
 def swap_case(s):
     new_s = s.swapcase()
