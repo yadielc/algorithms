a693ddb307570e9ef039195ae29b107e2db056a2
Yadiel <ycabr035@fiu.edu>
2018-08-23 16:44:54 -0400

    added whats your name solution

--- hackerRank/python/whatsyourname.py
+++ hackerRank/python/whatsyourname.py
@@ -2,6 +2,20 @@
 '''
 whatsyourname.py
 
+Instructions:
+
+Input Format
+
+The first line contains the first name, and the second line contains the last name.
+
+Constraints
+
+The length of the first and last name ≤ .
+
+Output Format
+
+Print the output as mentioned above.
+
 '''
 def print_full_name(first_name, last_name):
    print (("Hello %s %s! You just delved into python.") % (first_name, last_name))
