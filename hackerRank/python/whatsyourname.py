f1bc758f06b6d903ecd24cf9c84ae07a78b42ed4
Yadiel <ycabr035@fiu.edu>
2018-08-23 16:46:46 -0400

    added whats your name solution

--- hackerRank/python/whatsyourname.py
+++ hackerRank/python/whatsyourname.py
@@ -13,5 +13,6 @@ Output Format
 Print the output as mentioned above.
 
 '''
+
 def print_full_name(first_name, last_name):
    print (("Hello %s %s! You just delved into python.") % (first_name, last_name))
