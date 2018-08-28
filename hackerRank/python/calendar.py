e5d422fbd9a1ad483df340fec1eabe8624acd4f6
Yadiel <ycabr035@fiu.edu>
2018-08-28 16:27:48 -0400

    added calendar module solution

--- hackerRank/python/calendar.py
+++ hackerRank/python/calendar.py
@@ -9,7 +9,7 @@ You are given a date. Your task is to find what the day is on that date.
 
 import calendar # import calendar module
 
-date = raw_input().strip().split() # store user input 
+date = raw_input().strip().split() # store user input
 
-days = list(calendar.day_name)
+days = list(calendar.day_name) # store days in list 
 print days[calendar.weekday(int(date[2]), int(date[0]), int(date[1]))].upper()
