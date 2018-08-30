2dc379cec1c9480df0bd23721ba4234e2cad0b13
Yadiel <ycabr035@fiu.edu>
2018-08-30 13:09:27 -0400

    added string validators solution

--- hackerRank/python/stringvalidators.py
+++ hackerRank/python/stringvalidators.py
@@ -25,8 +25,8 @@ Review of Concepts:
 
 if __name__ == '__main__':
     s = raw_input()
-    print(any(c.isalnum() for c in str))
-    print(any(c.isalpha() for c in str))
-    print(any(c.isdigit() for c in str))
-    print(any(c.islower() for c in str))
-    print(any(c.isupper() for c in str))
+    print(any(c.isalnum() for c in s))
+    print(any(c.isalpha() for c in s))
+    print(any(c.isdigit() for c in s))
+    print(any(c.islower() for c in s))
+    print(any(c.isupper() for c in s))
