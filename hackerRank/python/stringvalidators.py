86a21d96467084e69744cc25f35a3add185e85e4
Yadiel <ycabr035@fiu.edu>
2018-08-30 12:53:27 -0400

    added string validators solution

--- hackerRank/python/stringvalidators.py
+++ hackerRank/python/stringvalidators.py
@@ -3,3 +3,19 @@ stringvalidators.py
 
 
 '''
+
+if __name__ == '__main__':
+    s = raw_input()
+    for i in s:
+        if i.isalnum():
+            print True
+        if i.isalpha():
+            print True
+        if i.isdigit():
+            print True
+        if i.islower():
+            print True
+        if i.isupper():
+            print True
+        else:
+            print False
