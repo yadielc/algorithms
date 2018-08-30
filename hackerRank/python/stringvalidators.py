a17cadea2888d09befa4fdcdc887e9c69ea8729f
Yadiel <ycabr035@fiu.edu>
2018-08-30 13:00:16 -0400

    added string validators solution

--- hackerRank/python/stringvalidators.py
+++ hackerRank/python/stringvalidators.py
@@ -3,6 +3,10 @@ stringvalidators.py
 
 String Validators HackerRank Problem Solution
 
+You are given a string .
+Your task is to find out if the string  contains: alphanumeric characters,
+ alphabetical characters, digits, lowercase and uppercase characters.
+
 '''
 
 if __name__ == '__main__':
