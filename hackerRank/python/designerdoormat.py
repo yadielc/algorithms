2e4cd78ea0f7831ff4647f920597df44807bea7d
Yadiel <ycabr035@fiu.edu>
2018-08-31 16:35:41 -0400

    added designer doormat solution

--- hackerRank/python/designerdoormat.py
+++ hackerRank/python/designerdoormat.py
@@ -10,9 +10,10 @@ for this.
 #get mat specs
 N, M = map(int, raw_input().split())
 
-#
+#display
 for i in xrange(1, N, 2):
     print str('.|.' * i).center(M, '-')
 print 'WELCOME'.center(M, '-')
+
 for i in xrange(N-2, -1, -2):
     print str('.|.' * i).center(M, '-')
