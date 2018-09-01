0936930d483966fad4a4a3c233e8bf1cd76b767d
Yadiel <ycabr035@fiu.edu>
2018-09-01 13:20:17 -0400

    added textwrap problem solution

--- hackerRank/python/textwrap.py
+++ hackerRank/python/textwrap.py
@@ -2,6 +2,9 @@
 textwrap.py
 Text Wrap HackerRank Problem Solution
 
+Instructions:
+You are given a string  and width .
+Your task is to wrap the string into a paragraph of width .
 '''
 
 def wrap(string, max_width):
