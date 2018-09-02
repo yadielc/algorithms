1e73f77d34c2617bf8f0971180aefb930b4a7c89
Yadiel <ycabr035@fiu.edu>
2018-09-02 17:24:55 -0400

    added exceptions problem solution

---
+++ hackerRank/python/exceptions.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+l = int(raw_input())
+
+for i in range(0, l):
+    try:
+        a, b = map(int, raw_input().split())
+        print a/b
+    except ZeroDivisionError as e:
+        print "Error Code:", e
+    except ValueError as e:
+        print "Error Code:", e
