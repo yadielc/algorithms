97674eaa08943b1b2529347f7ac42eb2e68cf980
Yadiel Cabrera <33431535+ysites@users.noreply.github.com>
2018-09-07 19:32:19 -0400

    Update product.py

--- hackerRank/python/product.py
+++ hackerRank/python/product.py
@@ -1,4 +1,10 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+product.py
+
+HackerRank itertools.product() Problem Solution 
+
+'''
 import sys,itertools
 list1= list(map(int,raw_input().split()))
 list2 = list(map(int,raw_input().split()))
