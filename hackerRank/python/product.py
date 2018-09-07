aa19b669b40a38414899f3337e272f81cecd5c0c
Yadiel Cabrera <33431535+ysites@users.noreply.github.com>
2018-09-07 19:31:14 -0400

    added itertools.product problem solution

---
+++ hackerRank/python/product.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys,itertools
+list1= list(map(int,raw_input().split()))
+list2 = list(map(int,raw_input().split()))
+print(' '.join(str(e) for e in itertools.product(list1,list2)))
