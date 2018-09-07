4a5b5d0a7476bc464a1dbd7b538d69af0d5e860c
Yadiel Cabrera <33431535+ysites@users.noreply.github.com>
2018-09-07 19:33:22 -0400

    Update product.py

--- hackerRank/python/product.py
+++ hackerRank/python/product.py
@@ -2,10 +2,13 @@
 '''
 product.py
 
-HackerRank itertools.product() Problem Solution 
+HackerRank itertools.product() Problem Solution
+https://www.hackerrank.com/challenges/itertools-product/problem
 
 '''
+
 import sys,itertools
+
 list1= list(map(int,raw_input().split()))
 list2 = list(map(int,raw_input().split()))
 print(' '.join(str(e) for e in itertools.product(list1,list2)))
