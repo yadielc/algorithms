b365b8f4128d2e1e35e1e58a1581d2a6fe1a857e
Yadiel <ycabr035@fiu.edu>
2018-09-12 16:43:13 -0400

    added listcomprehensions solution

---
+++ hackerRank/listcomprehensions.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    x = int(raw_input())
+    y = int(raw_input())
+    z = int(raw_input())
+    n = int(raw_input())
+
+    #List comprehension
+    arr = [[X, Y, Z] for X in range(x+1) for Y in range(y+1) for Z in range(z+1) if X + Y + Z !=n]
+
+    #Output the result
+    print(arr)
