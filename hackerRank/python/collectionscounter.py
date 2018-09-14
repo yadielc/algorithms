ae882a2c26189ef9cced7b4bc8406bcb613c5f85
Yadiel <ycabr035@fiu.edu>
2018-09-14 10:43:37 -0400

    added counter collections problem solution

--- hackerRank/python/collectionscounter.py
+++ hackerRank/python/collectionscounter.py
@@ -2,7 +2,11 @@
 collectionscounter.py
 https://www.hackerrank.com/challenges/collections-counter/problem
 
+Raghu is a shoe shop owner. His shop has X number of shoes. 
+He has a list containing the size of each shoe he has in his shop.
+There are N number of customers who are willing to pay  amount of money only if they get the shoe of their desired size.
 
+Your task is to compute how much money  earned.
 
 '''
 import collections
