fc75a5c9fae3d87e52f55ff8caae35904a7b5413
Yadiel <ycabr035@fiu.edu>
2018-09-20 17:54:01 -0400

    added set add problem solution

--- hackerRank/python/setadd.py
+++ hackerRank/python/setadd.py
@@ -2,6 +2,10 @@
 setadd.py
 
 Set .add() - HackerRank Problem Solution
+Apply your knowledge of the .add() operation to help your friend Rupal.
+
+Rupal has a huge collection of country stamps. She decided to count the total number of distinct country stamps in her collection. She asked for your help.
+You pick the stamps one by one from a stack of N country stamps.
 
 '''
 
